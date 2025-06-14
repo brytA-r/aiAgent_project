62a89d8c5639293755fedcde6f4c544812c33efc
Mad mask <Mdmsk@MacBook-Air-di-Bryt.local>
2025-06-14 21:17:57 +0200

    added Handoffs to agent

--- agentAndTool.js
+++ agentAndTool.js
@@ -1,21 +1,32 @@
 import 'dotenv/config';
 import { z } from 'zod';
-import {Agent, tool }  from '@openai/agents';
+import { Agent, run }  from '@openai/agents';
 
-const historyFunFact = tool ({
-    // The name of the tool will be used by the agent to tell what tool to use.
-    name: 'history_fun_fact',
-    // The description is used to describe **where** to use the tool by telling it **what** it does.
-    description: 'Give a fun fact about a historical event',
-    inputType: z.object({}),
-    execute: async () => {
-        // The output will be returned to the agent to use.
-        return 'Sharks are older than trees.';
-    },
-});
-const agent = new Agent({
+
+const historyTutorAgent = new Agent({
     name: 'History Tutor',
     instructions: 'You provide assistance with historical queries. Explain important events and context clearly.',
-    // Adding the tool to the agent.
-    tools: [historyFunFact],
-})
+});
+
+const mathTutorAgent = new Agent({
+    name: 'Math Tutor',
+    instructions: 
+    'You provide help with math problems. Explain your reasoning at each step and include examples.',
+});
+
+    // Handoffs
+const triageAgent = Agent.create({
+    name: 'Triage Agent',
+    instructions: 
+    "You determine which agent to use based on the user's homework question ",
+    handoffs: [historyTutorAgent, mathTutorAgent],
+});
+
+async function main() {
+    const result = await run (triageAgent, 'What is 2/6?');
+    console.log(result.finalOutput);
+}
+
+main().catch((err) => console.error(err));
+// Output: The capital of France is Paris.
+

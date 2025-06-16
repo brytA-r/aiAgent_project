7dd89be00e7c516d2ec523020f249837c4532d02
brytA-r <annorbright169@gmail.com>
2025-06-16 20:54:12 +0200

    add function to console log out the result

--- Agents/agentWithHandoffs.js
+++ Agents/agentWithHandoffs.js
@@ -1,4 +1,4 @@
-import { Agent } from '@openai/agents';
+import { Agent, run } from '@openai/agents';
 
 const bookingAgent = new Agent({
     name: 'Booking Agent',
@@ -13,10 +13,19 @@ const refundAgent = new Agent({
 const triageAgent = Agent.create({
     name: 'Triage Agent',
     instructions: [
-        'Help the user with their questions-',
+        'Help the user with their questions.',
         'If the user asks about bookup, hand off to the booking agent.',
         'If the user asks about refunds, hand off to the refund agent.',
     ].join('\n'),
     handoffs: [bookingAgent, refundAgent],
 });
+
+
+async function main (){
+    const result = await run(triageAgent, 'I would like a refund for my purchase.');
+    console.log(result.finalOutput);
+}
+
+main().catch(console.error);
+
 

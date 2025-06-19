f3d2695fffcd8ff778f31a22fb828617f32fa0d0
brytA-r <annorbright169@gmail.com>
2025-06-19 23:06:47 +0200

    update to json file, and new file carrying over conversations

---
+++ runningAgents/carryingOverConversation.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { Agent, AgentInputItem, run } from '@openai/agents';
+
+let thread: AgentInputItem[] = [];
+
+const agent = new Agent({
+    name: 'Assistant',
+});
+
+async function userSays(text: string) {
+    const result = await run(agent,
+        thread.concat({ role: 'user', content: text }),
+    );
+
+    thread = result.history;
+    console.log(`User: ${text}`);
+    console.log(`Assistant; ${result.finalOutput}`);
+    return result.finalOutput;
+}
+
+await userSays('What city is the Golden Bridge in?');
+
+await userSays('What state is it in?');

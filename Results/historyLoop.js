ae6627a08d8a25bf03c9ae62f4cc0d36a0d14540
brytA-r <annorbright169@gmail.com>
2025-06-24 22:02:43 +0200

    create new agent: Assistant

--- Results/historyLoop.js
+++ Results/historyLoop.js
@@ -1,2 +1,9 @@
 import 'dotenv/config';
 import {Agent, AgentInputItem, usesr, run } from '@openai/agents';
+
+const agent = new Agent({
+    name: 'Assistant',
+    instructions: 
+    'You are helpful assistant knowledgeable about recent AGI research.',
+});
+

9047264a0276a1425ec3c807fffd36836b81df76
brytA-r <annorbright169@gmail.com>
2025-06-25 20:56:25 +0200

    tool() helper

--- Tools/functionTools.js
+++ Tools/functionTools.js
@@ -1,3 +1,11 @@
 import { tool } from '@openai/agents';
 import { z } from 'zod';
 
+const getWeatherTool = too({
+    name: 'get_weather',
+    description: 'Get the weater for a given city',
+    parameters: z.object({ city: z.string() }),
+    async execute({ city} ) {
+        return `The weather in ${city} is sunny`;
+    },
+});

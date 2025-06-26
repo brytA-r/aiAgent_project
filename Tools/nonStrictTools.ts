a403c23b8b2ee3e95b2b0a1101315c4589189631
brytA-r <annorbright169@gmail.com>
2025-06-26 20:40:10 +0200

    add comment line

--- Tools/nonStrictTools.ts
+++ Tools/nonStrictTools.ts
@@ -15,6 +15,7 @@ const looseTool = tool({
     },
 
     execute: async (input) => {
+        // because strict is false we need to do our own verification
         if(typeof input !== 'object' || input === null || !('text' in input)) {
             return 'Invalid input. Please try again';
         }

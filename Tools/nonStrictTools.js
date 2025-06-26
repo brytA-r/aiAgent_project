bb0cec01d1bd7b772f6b7e20c473944b22425035
brytA-r <annorbright169@gmail.com>
2025-06-26 20:24:37 +0200

    create looseTool tool

--- Tools/nonStrictTools.js
+++ Tools/nonStrictTools.js
@@ -4,3 +4,13 @@ interface LooseToolInput {
     text: String;
 }
 
+const looseTool = tool({
+    description: 'Echo input; be forgiving about types',
+    strict: false,
+    parameters: {
+        type: 'object',
+        properties: { text: { type: 'string' }},
+        required: ['text'],
+        additionalProperties: true,
+    }
+});
